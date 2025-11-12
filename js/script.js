95bff6a9d394ea48a168f9c65782dbf20d642951
Andrea <andrea.cfb09@gmail.com>
2025-11-11 21:01:13 -0600

    ft:Se agrega ejemplo de eventos

---
+++ js/script.js
@@ -0,0 +1,55 @@
+
+//iteraciones
+const cielo=document.getElementById("cielo");
+
+for(let i=0; i<5 ;i++){
+    cielo.innerHTML +="⭐";
+}
+
+const planetas=["🌍", "🪐", "🌕"]
+for (const planeta of planetas){
+    cielo.innerHTML+=planeta;
+}
+
+let n=0
+while(n<5){
+    cielo.innerHTML +="☁️";
+    n++;
+}
+
+let m=0
+do{
+    cielo.innerHTML +="🚀"
+    m++;
+}while (m<4)
+
+
+
+const btnEstrella=document.getElementById("btnEstrella");
+
+btnEstrella.addEventListener("click", ()=>{
+    cielo.innerHTML +="⭐";
+
+})
+
+//doble click
+const btnPlaneta = document.getElementById("btnPlaneta");
+
+btnPlaneta.addEventListener("dblclick",()=>{
+    cielo.innerHTML += "🪐";
+
+})
+
+const inputEmoji = document.getElementById("inputEmoji");
+
+const btnAgregarEmoji = document.getElementById ("btnAgregarEmoji")
+
+btnAgregarEmoji.addEventListener("click", ()=> {
+    var emoji= inputEmoji.value.trim();
+    if (emoji) cielo.innerHTML +=emoji;
+    inputEmoji.value="";
+})
+
+inputEmoji.addEventListener("keydown", e=>{
+    if(e.key === "Escape") cielo.innerHTML="";
+})
